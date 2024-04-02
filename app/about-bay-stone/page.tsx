c48dbe12275c14214bf90274d7860d4ba440cbc1
developer-anil-magar <anilmagar.developer@gmail.com>
2024-04-02 15:25:58 +0545

    about page initialization

--- app/about-bay-stone/page.tsx
+++ app/about-bay-stone/page.tsx
@@ -1,5 +1,14 @@
-const page = () => {
-  return <div>About bay stone</div>;
+import { fetchData } from '@/queries/storyblokQueries';
+import StoryblokStory from '@storyblok/react/story';
+
+const page = async () => {
+  const { data } = await fetchData('about-bay-stone');
+  return (
+    <main className="size-full font-urbane">
+      about-bay-stone
+      <StoryblokStory story={data.story} />
+    </main>
+  );
 };
 
 export default page;
